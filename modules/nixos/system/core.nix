44b0ff7d994a3dc8ba5fe47915e43762a6cd1641
68Corolla <184780572+track-prepped-68-Corolla@users.noreply.github.com>
2026-05-09 22:26:14 -0400

    revert(core): remove substituters — wrong place, doesn't affect hm builds

--- modules/nixos/system/core.nix
+++ modules/nixos/system/core.nix
@@ -1,7 +1,6 @@
 { lib, pkgs, config, ... }:
 
 {
-  # --- 1. DECLARE THE OPTION HERE ---
   options.ft = {
     flakeDir = lib.mkOption {
       type = lib.types.str;
@@ -9,21 +8,10 @@
     };
   };
 
-  # --- 2. SET THE VALUES HERE ---
   config = {
     ft.flakeDir = "/home/joe/git/nixos-config";
 
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    nix.settings.substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-    ];
-
-    nix.settings.trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCUSPs4="
-    ];
 
     system.stateVersion = "24.11";
 
