2379785dc076eef6d87a9b64fa0433d55aa92901
68Corolla <184780572+track-prepped-68-Corolla@users.noreply.github.com>
2026-05-20 20:27:55 -0400

    Refactor CachyOS kernel options in kernel.nix

--- modules/nixos/system/kernel.nix
+++ modules/nixos/system/kernel.nix
@@ -1,10 +1,4 @@
-{
-  lib,
-  config,
-  pkgs,
-  inputs,
-  ...
-}:
+{ lib, config, pkgs, inputs, ... }:
 {
   options.ft.kernel.cachyos = {
     enable = lib.mkEnableOption "CachyOS optimized kernel" // {
@@ -12,26 +6,16 @@
     };
     variant = lib.mkOption {
       type = lib.types.enum [
-        "latest"
-        "latest-lto"
-        "bore"
-        "bore-lto"
-        "eevdf"
-        "eevdf-lto"
-        "bmq"
-        "bmq-lto"
-        "lts"
-        "lts-lto"
-        "rt-bore"
-        "rt-bore-lto"
-        "hardened"
-        "hardened-lto"
-        "server"
-        "server-lto"
-        "rc"
-        "rc-lto"
-        "deckify"
-        "deckify-lto"
+        "latest"     "latest-lto"
+        "bore"       "bore-lto"
+        "eevdf"      "eevdf-lto"
+        "bmq"        "bmq-lto"
+        "lts"        "lts-lto"
+        "rt-bore"    "rt-bore-lto"
+        "hardened"   "hardened-lto"
+        "server"     "server-lto"
+        "rc"         "rc-lto"
+        "deckify"    "deckify-lto"
       ];
       default = "latest";
       description = "CachyOS kernel variant. Maps to linux-cachyos-<variant> from nix-cachyos.";
@@ -39,8 +23,7 @@
   };
 
   config = lib.mkIf config.ft.kernel.cachyos.enable {
-    boot.kernelPackages =
-      pkgs.linuxPackagesFor
-        inputs.nix-cachyos.packages.${pkgs.stdenv.hostPlatform.system}."linux-cachyos-${config.ft.kernel.cachyos.variant}";
+    boot.kernelPackages = pkgs.linuxPackagesFor
+      inputs.nix-cachyos.packages.${pkgs.stdenv.hostPlatform.system}."linux-cachyos-${config.ft.kernel.cachyos.variant}";
   };
 }
